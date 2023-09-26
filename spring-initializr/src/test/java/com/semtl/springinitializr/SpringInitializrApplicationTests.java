2bc2a986c2793ff4432404c3fbbb97e4d64fbfac
hyunsu00 <hyun955807@naver.com>
2023-09-27 01:59:44 +0900

    spring-initializr 추가

---
+++ spring-initializr/src/test/java/com/semtl/springinitializr/SpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semtl.springinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
